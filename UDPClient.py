cd09d9ede6605f7f6597b0ce8b22fe178c440c18
Rene Street <20112809@post.au.dk>
2020-03-02 14:58:34 +0100

    Server done

--- UDPClient.py
+++ UDPClient.py
@@ -1,6 +1,5 @@
 import sys
 import socket
-from lib import Lib
 
 PORT = 9000
 BUFSIZE = 1000
@@ -8,7 +7,7 @@ BUFSIZE = 1000
 def main(argv):
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    clientSocket.sendto("l", (10.0.0.1, 9000))
+    clientSocket.sendto("l", ("10.0.0.1", 9000))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- UDPServer.py
+++ UDPServer.py
@@ -1,17 +1,37 @@
 import sys
 import socket
 
-HOST = '10.0.0.1'
+HOST = "10.0.0.1"
 PORT = 9000
-BUFSIZE = 1000
 
 def main(argv):
+
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind((HOST, PORT))
-    
+
     while True:
+        print('Server ready to receive')
         data, addr = serverSocket.recvfrom(1024)
-        print('Received message: ', data)
+        message = data.decode().upper()
+
+        if message == "U":
+            file = open("/proc/uptime", "r")
+            uptime = file.read()
+            uptimeStr = convertTuple(uptime)
+            serverSocket.sendto(uptimeStr.encode(), (addr[0], addr[1]))
+            file.close()
+        elif message == "L":
+            file = open("/proc/loadavg", "r")
+            loadavg = file.read()
+            loadavgStr = convertTuple(loadavg)
+            serverSocket.sendto(loadavgStr.encode(), (addr[0], addr[1]))
+            file.close()
+
+    serverSocket.close()
+
+def convertTuple(tup): 
+    str =  ''.join(tup) 
+    return str            
 
 if __name__ == "__main__":
    main(sys.argv[1:])
